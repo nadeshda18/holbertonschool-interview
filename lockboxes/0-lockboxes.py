36e40f5b4c56e1670af32be01acaa478e48f1753
nadeshda18 <nadeshda02@hotmail.com>
2024-05-08 23:40:46 -0400

    added comments

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,13 +8,21 @@ def canUnlockAll(boxes):
     boxes = list of lists
     Return = True if all boxes can be opened
     False if not all boxes can be opened"""
+    # box is empty, return false
     if len(boxes) == 0:
         return False
+    # box 0 is unlocked
     keys = [0]
+    # loop through keys
     for key in keys:
+        # loop through boxes
         for new_key in boxes[key]:
+            # if new_key is not in keys and new_key is less
+            # than the number of boxes
             if new_key not in keys and new_key < len(boxes):
+                # add new_key to keys
                 keys.append(new_key)
+    # if the number of keys is equal to the number of boxes
     if len(keys) == len(boxes):
         return True
     return False
