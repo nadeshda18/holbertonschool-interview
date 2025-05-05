ab8ade3dda5c7e852d08888a2b1442eb693c4166
nadeshda18 <nadeshda02@hotmail.com>
2025-05-05 19:43:23 -0400

    it's all about details

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -46,7 +46,7 @@ if (array[mid] == value)
 {
 if (mid == left || array[mid - 1] != value)
 return (mid);
-return (recursive_binary(array, left, mid, value));
+return (recursive_binary(array, left, mid -1, value));
 }
 else if (array[mid] > value)
 return (recursive_binary(array, left, mid - 1, value));
